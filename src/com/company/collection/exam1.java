4128ca2487ee9cd61601f027ae686e87f55fa4d6
DevLeeYG <hi33741@naver.com>
2022-07-25 10:53:48 +0900

    'Arraylist'

---
+++ src/com/company/collection/exam1.java
@@ -0,0 +1,35 @@
+package com.company.collection;
+
+
+
+import java.util.*;
+
+public class exam1 {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        String[] beer = {"Kloud", "Cass", "Asahi", "Guinness", "Heineken"};
+        list.add("java");
+        list.add("JDBC");
+        list.add("Servlet/jsp");
+        list.add(2,"Database");
+        list.add("iBatis");
+
+
+
+
+
+
+
+        for(int i = 0 ; i<list.size(); i++){
+
+            String str = list.get(i);
+
+        }
+
+
+
+    }
+
+
+}
